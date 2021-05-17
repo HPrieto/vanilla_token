2b30a72b4c35c1507f378eb4f665f394c109aab3
Heriberto Prieto <Duskayame@gmail.com>
2021-05-16 20:37:02 -0700

    GucciCoin complete, unit tests passing

--- test/GucciCoin.test.js
+++ test/GucciCoin.test.js
@@ -1,15 +1,18 @@
 const GucciCoin = artifacts.require('GucciCoin');
+const assertRevert = require('../util/test/assertRevert.js');
 
 contract('GucciCoin', accounts => {
   let token;
   const owner = accounts[0];
   const recipient = accounts[1];
   const allowedAccount = accounts[2];
-  const tokenTotalSupply = 300000000000000000000000000;
+  const tokenTotalSupply = new web3.utils.BN('300000000000000000000000000');
   const tokenName = "GucciCoin";
   const tokenSymbol = "GCC";
   const tokenDecimals = "18";
-  const ownerSupply = 300000000000000000000000000;
+  const ownerSupply = new web3.utils.BN('300000000000000000000000000');
+
+  const decimalsMul = new web3.utils.BN('1000000000000000000');
 
   beforeEach(async () => {
     token = await GucciCoin.new();
@@ -38,5 +41,91 @@ contract('GucciCoin', accounts => {
   it ('has correct owner token balance after construction', async () => {
     const actualBalance = await token.balanceOf(owner);
     assert.equal(actualBalance.toString(), ownerSupply.toString());
+  });
+
+  it ('recipient and sender have correct balances after transfer', async () => {
+    const tokenAmount = decimalsMul.mul(new web3.utils.BN('100'));
+    await token.transfer(recipient, tokenAmount);
+    const actualSenderBalance = await token.balanceOf(owner);
+    const actualRecipientBalance = await token.balanceOf(recipient);
+    assert.equal(actualSenderBalance.toString(), ownerSupply.sub(tokenAmount).toString());
+    assert.equal(actualRecipientBalance.toString(), tokenAmount.toString());
+  });
+
+  it ('emits Transfer event on transfer', async () => {
+    const tokenAmount = decimalsMul.mul(new web3.utils.BN('100'));
+    const { logs } = await token.transfer(recipient, tokenAmount);
+    const event = logs.find(e => e.event === 'Transfer');
+    assert.notEqual(event, undefined);
+  });
+
+  it ('throws when trying to transfer more than available balance', async () => {
+    const moreThanBalance = tokenTotalSupply.add(new web3.utils.BN('1'));
+    await assertRevert(token.transfer(recipient, moreThanBalance));
+  });
+
+  it ('returns the correct allowance amount after approval', async () => {
+    const tokenAmount = decimalsMul.mul(new web3.utils.BN('100'));
+    await token.approve(recipient, tokenAmount);
+    const actualAllowance = await token.allowance(owner, recipient);
+    assert.equal(actualAllowance.toString(), tokenAmount.toString());
+  });
+
+  it('emits Approval event after approval', async () => {
+    const tokenAmount = decimalsMul.mul(new web3.utils.BN('100'));
+    const { logs } = await token.approve(recipient, tokenAmount);
+    const event = logs.find(e => e.event === 'Approval');
+    assert.notEqual(event, undefined);
+  });
+
+  it('reverts if owner wants to reset allowance before setting it to 0 first', async () => {
+    const tokenAmount = decimalsMul.mul(new web3.utils.BN('100'));
+    const newTokenAmount = decimalsMul.mul(new web3.utils.BN('50'));
+    await token.approve(recipient, tokenAmount);
+    await assertRevert(token.approve(recipient, newTokenAmount));
+  });
+
+  it('successfully resets allowance', async () => {
+    const tokenAmount = decimalsMul.mul(new web3.utils.BN('100'));
+    const newTokenAmount = decimalsMul.mul(new web3.utils.BN('50'));
+    await token.approve(recipient, tokenAmount);
+    await token.approve(recipient, 0);
+    await token.approve(recipient, newTokenAmount);
+    const actualAllowance = await token.allowance(owner, recipient);
+    assert.equal(actualAllowance.toString(), newTokenAmount.toString());
+  });
+
+  it('returns correct balances after transfering from another account', async () => {
+    const tokenAmount = decimalsMul.mul(new web3.utils.BN('100'));
+    await token.approve(allowedAccount, tokenAmount);
+    await token.transferFrom(owner, recipient, tokenAmount, { from: allowedAccount });
+    const balanceOwner = await token.balanceOf(owner);
+    const balanceRecipient = await token.balanceOf(recipient);
+    const balanceAllowedAcc = await token.balanceOf(allowedAccount);
+    assert.equal(balanceOwner.toString(), ownerSupply.sub(tokenAmount).toString());
+    assert.equal(balanceAllowedAcc.toNumber(), 0);
+    assert.equal(new web3.utils.BN(balanceRecipient), tokenAmount.toString());
+  });
+
+  it('emits Transfer event on transferFrom', async () => {
+    const tokenAmount = decimalsMul.mul(new web3.utils.BN('100'));
+    await token.approve(allowedAccount, tokenAmount);
+    const { logs } = await token.transferFrom(owner, recipient, tokenAmount,
+      { from: allowedAccount });
+    const event = logs.find(e => e.event === 'Transfer');
+    assert.notEqual(event, undefined);
+  });
+
+  it('throws when trying to transferFrom more than allowed amount', async () => {
+    const tokenAmountAllowed = decimalsMul.mul(new web3.utils.BN('99'));
+    const tokenAmount = decimalsMul.mul(new web3.utils.BN('100'));
+    await token.approve(allowedAccount, tokenAmountAllowed);
+    await assertRevert(token.transferFrom(owner, recipient, tokenAmount, { from: accounts[1] }));
+  });
+
+  it('throws an error when trying to transferFrom more than _from has', async () => {
+    await token.approve(allowedAccount, ownerSupply.add(new web3.utils.BN('1')));
+    await assertRevert(token.transferFrom(owner, recipient, ownerSupply.add(new web3.utils.BN('1')),
+      { from: allowedAccount}));
   });
 });
